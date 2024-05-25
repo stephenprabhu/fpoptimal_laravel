9c18fdfabfeadd68a8eb5e9f98cd8bf57a5c1141
stephenprabhu <stephenmachado1998@gmail.com>
2024-05-25 17:21:54 +0400

    added cors middleware

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FPController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/check_fingerprint', [FPController::class, 'checkFingerprint']);
+Route::post('/finishPage', [FPController::class, 'finishPage']);
+Route::post('/getCookie', [FPController::class, 'getCookie']);
+Route::post('/check_exist_picture', [FPController::class, 'checkExistPicture']);
+Route::post('/pictures', [FPController::class, 'storePictures']);
+Route::post('/updateFeatures', [FPController::class, 'updateFeatures']);
+
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,3 @@
 use App\Http\Controllers\FPController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/check_fingerprint', [FPController::class, 'checkFingerprint']);
-Route::post('/finishPage', [FPController::class, 'finishPage']);
-Route::post('/getCookie', [FPController::class, 'getCookie']);
-Route::post('/check_exist_picture', [FPController::class, 'checkExistPicture']);
-Route::post('/pictures', [FPController::class, 'storePictures']);
-Route::post('/updateFeatures', [FPController::class, 'updateFeatures']);
